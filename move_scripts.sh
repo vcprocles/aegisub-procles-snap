bb5bf68fecac8e539933bae102f41ebd409e70f8
vcprocles <84681120+vcprocles@users.noreply.github.com>
2022-09-19 23:07:09 +0300

    fix access denied by not trying to reach into ~

--- move_scripts.sh
+++ move_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Creating directory for scripts ~/aegisub-lua"
-mkdir $SNAP_REAL_HOME/aegisub-lua/
+echo "Creating directory for scripts $HOME/aegisub-lua"
+mkdir $HOME/lua/
 echo "Moving default scripts..."
-cp -r $SNAP/usr/local/share/aegisub/automation/* $SNAP_REAL_HOME/aegisub-lua/
-echo "Now go into Aegisub Preferences -> Automation and change all the paths. You can remove and rename aegisub-lua as you please, as long as you don't make it hidden. If you want to hide it, drop it in ~/snap/aegisub-procles/current/"
+cp -r $SNAP/usr/local/share/aegisub/automation/* $HOME/lua/
+echo "Now go into Aegisub Preferences -> Automation and change all the paths. You can move this folder into another location, as long as it's not hidden."
 
